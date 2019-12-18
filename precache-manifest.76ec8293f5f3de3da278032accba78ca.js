a060d9ded53af0fe38e7cfc7653bf2d77196cc9d
RolandMarkarov <markarovr@gmail.com>
2019-12-18 19:18:10 +0300

    Updates

---
+++ precache-manifest.76ec8293f5f3de3da278032accba78ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc4e092e0ed3a9e4f182f16392db8f5",
+    "url": "/square-game/index.html"
+  },
+  {
+    "revision": "8d9f501111f75b03864d",
+    "url": "/square-game/static/css/main.170f8677.chunk.css"
+  },
+  {
+    "revision": "e3139028e02acc7ea6a6",
+    "url": "/square-game/static/js/2.3b45e196.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/square-game/static/js/2.3b45e196.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d9f501111f75b03864d",
+    "url": "/square-game/static/js/main.63be8072.chunk.js"
+  },
+  {
+    "revision": "f09c91453f692d8cf081",
+    "url": "/square-game/static/js/runtime-main.ecad3d3c.js"
+  }
+]);
